4703cd52e06905e54dc9459c05ee68b51eba7a71
pushkarsinghTTN <pushkarsingh@tothenew.com>
2018-03-27 19:49:19 +0530

    Q1,Q2 Add User domain class with fields and add constraints

---
+++ linksharing/grails-app/domain/User/User.groovy
@@ -0,0 +1,39 @@
+package User
+
+class User{
+    String firstname
+    String lastname
+    String username
+    String password
+    String email
+    Byte photo
+    Boolean admin
+    Boolean active
+    Date lastUpdated
+    Date dateCreated
+
+    User(String firstname, String lastname, String username, String password, String email, Byte photo, Boolean admin, Boolean active) {
+        this.firstname = firstname
+        this.lastname = lastname
+        this.username = username
+        this.password = password
+        this.email = email
+        this.photo = photo
+        this.admin = admin
+        this.active = active
+    }
+    //static hasMany = [topics: Topic, subscriptions: Subscription, readingItems: ReadingItem, resources: Resource]
+
+    static constraints = {
+        email(email: true, nullable: false, blank: false)
+        password(nullable: false, blank: false, size: 5..15)
+        firstname(nullable: false, blank: false)
+        lastname(nullable: false, blank: false)
+        username(nullable: false, blank: false)
+        photo(nullable: true)
+        admin(nullable: true)
+        active(nullable: true)
+    }
+
+
+}
