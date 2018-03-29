ba58d9cf5bd8134926efeb21a80b6f546fe38539
pushkarsinghTTN <pushkarsingh@tothenew.com>
2018-03-29 16:42:27 +0530

    Q8 If user is not found then flash.error is set to 'User not found' and flash.error is rendered

--- linksharing/grails-app/controllers/linksharing/LoginController.groovy
+++ linksharing/grails-app/controllers/linksharing/LoginController.groovy
@@ -3,32 +3,41 @@ package linksharing
 import user.User
 
 class LoginController {
+    static defaultAction = "loginhandler"
 
     def index() {
         if (session.user) {
             forward(controller: "User", action: "index")
         } else {
-            if(!flash.error)
-                redirect(action:"index")
+            if (!flash.error)
+                redirect(action: "index")
         }
     }
 
     def loginhandler(String username, String password) {
         User user1 = User.findByUsernameAndPassword(username, password)
-        if(user1){
+        if (user1) {
             if (user1.active) {
                 session.user = user1
-            }
-            else {
+            } else {
                 flash.error = "YOUR ACCOUNT IS NOT ACTIVE"
             }
-            redirect(action:"index")
+            redirect(action: "index")
+        } else {
+            if (!username || !password) {
+                flash.error = "PLEASE LOGIN"
+                redirect(action: "loginhandler")
+            } else {
+                flash.error = "USER NOT FOUND"
+                redirect(action: "index")
+            }
         }
 
     }
 
     def logout() {
         session.invalidate()
+        flash.error = "USER LOGGED OUT"
         forward(controller: "login", action: "index")
     }
 }
