276f4d47e86b37f1b21a79cf91b74c2f8a90ed9d
pushkarsinghTTN <pushkarsingh@tothenew.com>
2018-03-30 10:50:20 +0530

    D2 Q4 Exception of object not found should be handled in resource delete

--- linksharing/grails-app/controllers/resource/ResourceController.groovy
+++ linksharing/grails-app/controllers/resource/ResourceController.groovy
@@ -2,12 +2,17 @@ package resource
 
 class ResourceController {
 
-    def index() { }
+    def index() {}
 
     def delete(Integer resourceid) {
         Resource proxyresource = Resource.load(resourceid)
-        proxyresource.discard()
-        proxyresource.delete(flush:true)
-        render("Deleted Successfully")
+        if (!proxyresource) {
+            throw new Exception("NO SUCH RECORD IN OUR DATABASE")
+        } else {
+            proxyresource.discard()
+            proxyresource.delete(flush: true)
+            render("Deleted Successfully")
+        }
     }
+
 }
